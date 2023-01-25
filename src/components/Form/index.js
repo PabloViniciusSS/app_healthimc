4b33981e71c792035e44994c9c3aea43aa6e900d
Pablo Vinícius Sousa Silva <48532322+PabloViniciusSS@users.noreply.github.com>
2023-01-25 09:50:33 -0300

    criando as logicas

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,8 +1,33 @@
 import { View, Text, TextInput, Button } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import ResultImc from './ResultIMc'
 
-export default function Form({messageIMC, imc}) {
+export default function Form() {
+
+  const [height, setHeight] = useState(null)
+  const [weight, setWeight] = useState(null)
+  const [messageImc, setMessageImc] = useState("Preencha seu peso e altura")
+  const [imc, setImc] = useState(null)
+  const [textButton, setTextButton] = useState("Calcular")
+
+  function imcCalculator(){
+    return setImc((weight/(height * height)).toFixed(2))
+  }
+
+  function validationIMC(){
+    if(weight != null && height != null){
+      imcCalculator()
+      setHeight(null)
+      setWeight(null)
+      setMessageImc("Seu imc é igual: ")
+      setTextButton("Calcular Novamente")
+      return
+    }else
+    setImc(null)
+    setTextButton("Calcular")
+    setMessageImc("Preencha o peso e altura")
+  }
+
   return (
     <View>
         <View>
